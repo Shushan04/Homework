286ae4ddf6d7e9da6fd54f47939390bdff457ffb
Shushan04 <132073763+Shushan04@users.noreply.github.com>
2023-05-10 21:29:00 +0400

    Add files via upload

---
+++ Counter/index.js
@@ -0,0 +1,24 @@
+
+const span = document.querySelector('.value');
+const decrement = document.getElementById('btn1');
+const increment = document.getElementById('btn2');
+const reset = document.getElementById('.btn reset');
+
+
+let number = 0;
+
+decrement.addEventListener('click', function(){
+    span.textContent = number--
+    span.style.color = 'red'
+});
+
+increment.addEventListener('click', function(){
+    span.textContent = number++
+    span.style.color = 'green'
+ });
+
+//  reset.addEventListener('click', function (){
+//     number = 0;
+//     span.innerHTML = number;
+//  })
+
